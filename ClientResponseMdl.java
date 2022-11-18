185051c974dcce012804bad73b532098557943af
fanchuyao27 <92429920+fanchuyao27@users.noreply.github.com>
2022-11-18 16:10:47 +0800

    Update ClientResponseMdl.java

--- ClientResponseMdl.java
+++ ClientResponseMdl.java
@@ -3,12 +3,6 @@ package sand.model.mqtt;
 import lombok.Data;
 import com.google.gson.JsonObject;
 
-/**
- * @author fcy
- * @version 1.0.0
- * @description
- * @createTime 2022/9/16 18:01
- **/
 
 @Data
 public class ClientResponseMdl {
